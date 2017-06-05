976a2e9b5e4191fc03ea650fca04d34bdd18b290
panda-satyabrata <satyabrat.panda97@gmail.com>
2017-06-05 11:55:43 +0530

    Update Test_1.py

--- Test_1.py
+++ Test_1.py
@@ -3,7 +3,6 @@ from unittest import TestCase
 class TestComplex(TestCase):
     def test_sumofcomplex(self):
         from Complex import Complex
+        
         result = Complex(1,1) + Complex(2,2)
-        if isinstance(result, Complex):
-            self.assertEqual(result, 3+3i)
-            self.assertTrue(succeed("Complex"))
+        self.assertTrue(result, 3+3i)

002d578c37f9dc056d8382985d6cdfd6953d7025
panda-satyabrata <satyabrat.panda97@gmail.com>
2017-06-05 11:27:25 +0530

    Update Test_1.py

--- Test_1.py
+++ Test_1.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 class TestComplex(TestCase):
     def test_sumofcomplex(self):
-        from Complex import angle
+        from Complex import Complex
         result = angle(Complex(1,1))
         if isinstance(result, dict):
             self.assertEqual(result, 45)
